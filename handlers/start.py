1a256cc8451edd4437d3527be61d09d0aa35fcc8
nzeperica-maker <nzeperica@gmail.com>
2026-06-08 01:28:20 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,24 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ContextTypes
 
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    keyboard = [
+        ["🌅 Утренний настрой"],
+        ["🌙 Вечерняя инвентаризация"],
+        ["😨 Инвентаризация страхов"],
+        ["💢 Инвентаризация обид"],
+        ["📚 Духовные принципы"],
+        ["📈 Моё выздоровление"],
+        ["📊 Карточка духовного роста"],
+        ["⚙️ Настройки"]
+    ]
+
+    await update.message.reply_text(
+        "Добро пожаловать в Моё Выздоровление 🙏",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True
+        )
+    )
