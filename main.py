73c0a5116816563f040b0cd5a93f8bec41b2f369
nzeperica-maker <nzeperica@gmail.com>
2026-06-08 01:33:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,32 @@ from telegram.ext import (
     CommandHandler,
     ContextTypes
 )
-
 import os
 
 from handlers.morning import morning_checkin
 from handlers.evening import evening_review
-from handlers.fears import fear_inventory
-from handlers.resentments import resentment_inventory
+
+TOKEN = os.getenv("Bot_token")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🙏 Добро пожаловать в Моё выздоровление\n\n"
+        "Доступные команды:\n\n"
+        "/morning - Утренний настрой\n"
+        "/evening - Вечерняя инвентаризация"
+    )
+
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("morning", morning_checkin))
+    app.add_handler(CommandHandler("evening", evening_review))
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
