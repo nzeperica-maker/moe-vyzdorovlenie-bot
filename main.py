c573dd5f6e9e9e630b4e8832d6d89233095cd047
nzeperica-maker <nzeperica@gmail.com>
2026-06-08 00:37:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("Bot_token")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот работает!")
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
