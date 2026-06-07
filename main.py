2456a9a42c9173dc35e75bcf0e4b64e8e59763d0
nzeperica-maker <nzeperica@gmail.com>
2026-06-08 01:56:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telegram.ext import (
 )
 import os
 
-from handlers.morning import morning_checkin
+from handlers.morning import morning_checkin, user_states
 from handlers.evening import evening_review
 
 TOKEN = os.getenv("Bot_token")
@@ -22,14 +22,57 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "/evening - Вечерняя инвентаризация"
     )
 
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
 
+    if user_id not in user_states:
+        return
+
+    state = user_states[user_id]
+    text = update.message.text
+
+    if state["step"] == 1:
+        state["answers"]["score"] = text
+        state["step"] = 2
+
+        await update.message.reply_text(
+            "2. Какое у тебя сейчас настроение?"
+        )
+
+    elif state["step"] == 2:
+        state["answers"]["mood"] = text
+        state["step"] = 3
+
+        await update.message.reply_text(
+            "3. Есть ли сегодня тяга?\n"
+            "(нет / слабая / сильная)"
+        )
+
+    elif state["step"] == 3:
+        state["answers"]["craving"] = text
+        state["step"] = 4
+
+        await update.message.reply_text(
+            "4. Что поможет тебе сохранить трезвость сегодня?"
+        )
+
+    elif state["step"] == 4:
+        state["answers"]["plan"] = text
+
+        await update.message.reply_text(
+            "✅ Утренний настрой завершён!"
+        )
+
+        del user_states[user_id]
 def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("morning", morning_checkin))
     app.add_handler(CommandHandler("evening", evening_review))
-
+app.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
+)
     app.run_polling()
 
 
